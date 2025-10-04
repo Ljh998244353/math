c9dfd4a7b06c4cc9b6b0b0392980c06909a84602
Ljh998244353 <liu_jh2023@qq.com>
2025-10-04 13:51:52 +0800

    upd LIS

--- LaTex/newTemplate/source/LIS.cpp
+++ LaTex/newTemplate/source/LIS.cpp
@@ -7,6 +7,7 @@ int main() {
   vector<int> a(n + 1);
   vector<int> stk(n + 10);
   int top = 0, ans = 0;
+  vector<int> pre(n + 1);
   // 最长不上升子序列
   for (int i = 1; i <= n; ++i) {
     auto pos = lower_bound(stk.begin() + 1, stk.begin() + 1 + top, a[i],
@@ -15,6 +16,8 @@ int main() {
     if (pos > top) top++;
     stk[pos] = a[i];
     ans = max(ans, top);
+    // 序列恢复
+    if (top > 1) pre[i] = stk[top - 1];
   }
   cout << ans << "\n";
   top = 0, ans = 0;
